7ea5dd350adf62fcdb0ee03df919a585acdbbbcf
Viktor Selmeci <viktor_selmeci@epam.com>
2026-02-17 10:43:14 +0100

    unused imports

--- src/main/java/com/epam/assessment/knowledge/logic/strategies/QuestionSelectionStrategy.java
+++ src/main/java/com/epam/assessment/knowledge/logic/strategies/QuestionSelectionStrategy.java
@@ -1,6 +1,5 @@
 package com.epam.assessment.knowledge.logic.strategies;
 
-import com.epam.assessment.knowledge.model.KnowledgeBank;
 import com.epam.assessment.knowledge.model.KnowledgeElement;
 
 import java.util.List;
